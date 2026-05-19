28bccb2f1e66c63453479dbb07dc30d6654470ad
yumnaa-isaacs <yumnaaisaacs@gmail.com>
2026-05-19 21:51:55 +0200

    Add files via upload

---
+++ week6_ex01_variables.js
@@ -0,0 +1,17 @@
+// Create variables
+const firstName = "Yumnaa";
+const lastName = "Isaacs";
+
+let age = 20;
+
+// Combine first and last name
+const fullName = firstName + " " + lastName;
+
+// Print greeting
+console.log("Hello, my name is " + fullName);
+console.log("I am " + age + " years old.");
+
+// Change age value
+age = 21;
+
+console.log("Next year I will be " + age + " years old.");
