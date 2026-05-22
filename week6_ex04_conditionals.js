843c4ee9a2c29f8d9dd271b03dd2f43b20ffc5cc
yumnaa-isaacs <yumnaaisaacs@gmail.com>
2026-05-22 22:42:15 +0200

    Add files via upload

---
+++ week6_ex04_conditionals.js
@@ -0,0 +1,17 @@
+// Create a variable called score and give it a value between 0 and 100
+let score = 100;
+
+// Create a variable called passingGrade and set it to 60
+let passingGrade = 60;
+
+// Check if the score is perfect
+if (score === 100) {
+console.log("Perfect score!");
+}
+
+// Check if the student passed or failed
+if (score >= passingGrade) {
+console.log("Passed");
+} else {
+console.log("Failed");
+}
